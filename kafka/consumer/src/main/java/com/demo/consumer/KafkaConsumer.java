be78c58d2c310bd38a0ea9ccadedcab250bd95cb
BH20584 <salman.asad@macys.com>
2026-03-15 15:22:23 +0500

    spring cloud function

--- kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -6,8 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
-    public void listen(String message){
-        System.out.println("Received message: " + message);
+//    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+//    public void listen1(String message){
+//        System.out.println("Received message 1: " + message);
+//    }
+//
+//    @KafkaListener(topics = "my-topic", groupId = "my-new-group-1")
+//    public void listen2(String message){
+//        System.out.println("Received message 2: " + message);
+//    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group-rider")
+    public void listen2(RiderLocation riderLocation){
+        System.out.println("Received location: " + riderLocation.getRiderId() + " : " +
+                riderLocation.getLatitude() + " : " + riderLocation.getLongitude());
     }
 }
--- kafka/producer/src/main/java/com/demo/producer/KafkaProducer.java
+++ kafka/producer/src/main/java/com/demo/producer/KafkaProducer.java
@@ -8,17 +8,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class KafkaProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, RiderLocation> kafkaTemplate;
 
     @Autowired
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducer(KafkaTemplate<String, RiderLocation> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String message) {
-        kafkaTemplate.send("my-topic", message);
-        return "Message sent: " + message;
+        RiderLocation riderLocation = new RiderLocation("rider123", 28.61, 77.23);
+        //kafkaTemplate.send("my-topic", message);
+        kafkaTemplate.send("my-topic", riderLocation);
+        return "Message sent: " + riderLocation.getRiderId();
     }
 
 }
